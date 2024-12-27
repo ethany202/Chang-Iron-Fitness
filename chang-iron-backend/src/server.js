10d072337631c6ea54401c12fbfcac08b13cf303
Phoebe Qian <pq29@cornell.edu>
2024-12-27 01:01:31 -0500

    moved port number to env file and updated database names

--- chang-iron-backend/src/server.js
+++ chang-iron-backend/src/server.js
@@ -2,16 +2,47 @@ const express = require('express');
 const connectToDatabase = require('./mongoConnection');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+// Middleware for parsing JSON bodies
+app.use(express.json());
 
 async function startServer() {
-  const client = await connectToDatabase(); 
+  const client = await connectToDatabase();
+  const db = client.db("Test");
+  const collection = db.collection("users");
 
   app.get('/', async (req, res) => {
-    const db = client.db("your_database_name");
-    const collection = db.collection("your_collection_name");
     const data = await collection.find({}).toArray();
     res.send(data);
+  });
+
+  app.post("/addUser", async (req, res) => {
+    try {
+      const { email, password, membershipStatus, previousTransactions } = req.body;
+
+      // Validation
+      if (!email || !password || !membershipStatus || !previousTransactions) {
+        return res.status(400).json({ error: "All fields are required." });
+      }
+
+      // Create a new document
+      const newUser = {
+        email,
+        password,
+        membershipStatus,
+        previousTransactions,
+      };
+      const result = await collection.insertOne(newUser);
+
+      res.status(201).json({
+        message: "User added successfully",
+        userId: result.insertedId,
+      });
+    } catch (error) {
+      console.error("Error adding user:", error);
+      res.status(500).send("Error adding user", error);
+    }
   });
 
   app.listen(port, () => {
